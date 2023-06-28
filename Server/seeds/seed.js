6b1f809d007e3434c4bcb561a4e26f130b710954
m-vanhoose <m-vanhoose@live.com>
2023-06-27 21:27:59 -0400

    created seeds folder, begun seed files

---
+++ Server/seeds/seed.js
@@ -0,0 +1,17 @@
+const db = require('../config/connection');
+const { User, Pets} = require('../models');
+
+const userData = require('./userData.json');
+const petsData = require('./petsData.json');
+
+
+db.once('open', async () => {
+    await User.deleteMany({});
+    await Pets.deleteMany({});
+
+    const schools = await User.insertMany(userData);
+    const pets = await Pets.insertMany(petsData);
+
+    console.log('Seeding completed');
+    process.exit(0);
+})
