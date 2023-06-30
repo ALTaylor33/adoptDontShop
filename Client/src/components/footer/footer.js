adbdf56486344facd2feb696a3e55c1a0425ddc4
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-06-29 20:58:47 -0400

    Footer Added

--- Client/src/components/footer/footer.js
+++ Client/src/components/footer/footer.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+  MDBInput
+} from 'mdb-react-ui-kit';
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>
-      <h1>This is a footer</h1>
-    </div>
-  );
-};
+    <MDBFooter bgColor='light' className='text-center text-lg-left'>
+      <MDBContainer className='p-4 pb-0'>
+        <form action=''>
+          <MDBRow className='d-flex justify-content-center'>
+            <MDBCol size='auto' className='mb-4 mb-md-0'>
+              <p className='pt-2'>
+                <strong>Sign up for our newsletter</strong>
+              </p>
+            </MDBCol>
 
-export default Footer;
+            <MDBCol md='5' size='12' className='mb-4 mb-md-0'>
+              <MDBInput type='text' id='form5Example2' label='Email address' />
+            </MDBCol>
+
+            <MDBCol size='auto' className='mb-4 mb-md-0'>
+              <MDBBtn>Subscribe</MDBBtn>
+            </MDBCol>
+          </MDBRow>
+        </form>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        &copy; {new Date().getFullYear()} Copyright:{' '}
+        <a className='text-dark' href='https://mdbootstrap.com/'>
+          Adopt Dont Shop
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
