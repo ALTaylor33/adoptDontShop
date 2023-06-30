be84c23900e986ceebe844fd78308ffd3c66d31b
m-vanhoose <m-vanhoose@live.com>
2023-06-29 20:26:15 -0400

    added bcrypt functionality began filling out seeds

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     name: {
@@ -8,7 +9,8 @@ const userSchema = new Schema({
       email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address']
       },
       password: {
         type: String,
@@ -32,7 +34,19 @@ const userSchema = new Schema({
         {
         type: integer
     }
-})
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
