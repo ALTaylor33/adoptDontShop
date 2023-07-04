bc878df89a309cdd0fc0ef5f67e8f525063da1e4
ALTaylor33 <austin.taylor33@rocketmail.com>
2023-07-04 00:52:31 -0400

    donations, search results, users, donation react pages

---
+++ Client/src/components/donation/donation.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Card, Form, Button } from 'react-bootstrap';
+import { FaPaw } from 'react-icons/fa';
+
+const DonationPage = () => {
+  const [donationAmount, setDonationAmount] = useState('');
+
+  const handleDonation = async (event) => {
+    event.preventDefault();
+    // Handle donation submission and server-side integration with Stripe
+    // Make a POST request to your server endpoint with the donation details
+  };
+
+  return (
+    <div className="container">
+      <Card>
+        <Card.Body>
+          <Card.Title>Make a Donation</Card.Title>
+          <Form onSubmit={handleDonation}>
+            <Form.Group controlId="donationAmount">
+              <Form.Label>Donation Amount</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter donation amount"
+                value={donationAmount}
+                onChange={(event) => setDonationAmount(event.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="cardDetails">
+              <Form.Label>Card Details</Form.Label>
+              {/* Use the <CardElement /> component from `react-stripe-elements` here */}
+            </Form.Group>
+            <Button variant="primary" type="submit" className="paw-print-button">
+              <FaPaw className="paw-icon" /> Donate
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default DonationPage;
