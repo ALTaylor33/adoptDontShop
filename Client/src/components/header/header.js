04aeca77bc79f0ac033593b7900188f1d6b86925
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-07-10 19:05:17 -0400

    Tons of stuff

--- Client/src/components/header/header.js
+++ Client/src/components/header/header.js
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const Header = () => {
-  return (
-    <div>
-      <h1>Adopt Dont Shop</h1>
-    </div>
-  );
-};
+// const Header = () => {
+//   return (
+//     <div>
+//       <h1>Adopt Dont Shop</h1>
+//     </div>
+//   );
+// };
 
-export default Header;
+// export default Header;
