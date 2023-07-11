01b1327638160c3190e07c46a735e978d9a2e49f
Are-Jae <evansraven7@gmail.com>
2023-07-10 21:37:27 -0400

    debugging

--- Client/src/components/navbar/navbar.js
+++ Client/src/components/navbar/navbar.js
@@ -122,36 +122,4 @@ export default Navigationbar;
 
 
 
-    // <Navbar expand="lg" className="bg-body-tertiary">
- 
-    //         <Nav.Link href="#action1">Home</Nav.Link>
-    //         <Nav.Link href="#action2">Link</Nav.Link>
-    //         <NavDropdown title="Link" id="navbarScrollingDropdown">
-    //           <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-    //           <NavDropdown.Item href="#action4">
-    //             Another action
-    //           </NavDropdown.Item>
-    //           <NavDropdown.Divider />
-    //           <NavDropdown.Item href="#action5">
-    //             Something else here
-    //           </NavDropdown.Item>
-    //         </NavDropdown>
-    //         <Nav.Link href="#" disabled>
-    //           Link
-    //         </Nav.Link>
-    //       </Nav>
-    //       <Form className="d-flex">
-    //         <Form.Control
-    //           type="search"
-    //           placeholder="Search"
-    //           className="me-2"
-    //           aria-label="Search"
-    //         />
-    //         <Button ">Search</Button>
-    //       </Form>
-     
-   
-
-
-
 
