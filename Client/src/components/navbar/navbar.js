22212bd089b8db150ee478d2d59aea9be994f7b5
Are-Jae <evansraven7@gmail.com>
2023-07-13 01:32:47 -0400

    added logo to navbar

--- Client/src/components/navbar/navbar.js
+++ Client/src/components/navbar/navbar.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
-
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
-
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
 
 const NavbarComponent = ({ handleSearch }) => {
   const navigate = useNavigate();
@@ -61,61 +58,70 @@ const NavbarComponent = ({ handleSearch }) => {
       gender: genderFilter,
       radius: radiusFilter
     };
-        // generate a string in a key=value format, ex type=cat&location=texas
-        const filtersArray = []
-        for (let key in filters) {
-          filtersArray.push(`${key}=${filters[key]}`)
-        }
-        const searchFilters = filtersArray.join('&')
-        console.log(searchFilters)
-        //navigate to the searchresults page with query parameters
-        navigate({
-          pathname: '/searchResults',
-          search: `?location=${searchTerm}&${searchFilters}`
-        })
+
+    const filtersArray = [];
+    for (let key in filters) {
+      filtersArray.push(`${key}=${filters[key]}`);
+    }
+    const searchFilters = filtersArray.join('&');
+    console.log(searchFilters);
+
+    navigate({
+      pathname: '/searchResults',
+      search: `?location=${searchTerm}&${searchFilters}`
+    });
   };
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-    <Container fluid>
-  <Navbar.Brand href="/">adoptDontShop</Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-
-  <Nav
-      className="ml-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-
-
-  <Nav.Link className="ui active button" href="/">Home</Nav.Link>
-  <Nav.Link className="ui active button" href="/donate">Donate</Nav.Link>
-  <Nav.Link className="ui active button" href="/user">My Profile</Nav.Link>
-  <Nav.Link className="ui active button" href="/contact">Contact</Nav.Link>
-   
-    <Form className='float-right' onSubmit={handleSearchSubmit}>
-        <input
-          type="text"
-          placeholder="Search pets..."
-          value={searchTerm}
-          onChange={handleSearchInput}
-        />
-        <select name="type" value={typeFilter} onChange={handleFilterChange}>
-          <option value="">Species</option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="rabbit">Rabbit</option>
-          <option value="reptile">Reptile</option>
-        </select>
-        <Button type="submit" variant="outline-success">Search</Button>
-      </Form>
-      </Nav>
-      </Navbar.Collapse>
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <img
+            src="../assets/img/fullLogo.jpg"
+            alt="Logo"
+            className="logo-image"
+            style={{ width: '100px', height: 'auto' }} 
+          />
+          Adopt Don't Shop
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+          <Nav className="ml-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Nav.Link className="ui active button" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="ui active button" href="/donate">
+              Donate
+            </Nav.Link>
+            <Nav.Link className="ui active button" href="/user">
+              My Profile
+            </Nav.Link>
+            <Nav.Link className="ui active button" href="/contact">
+              Contact
+            </Nav.Link>
+            <Form className="float-right" onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                placeholder="Search pets..."
+                value={searchTerm}
+                onChange={handleSearchInput}
+              />
+              <select name="type" value={typeFilter} onChange={handleFilterChange}>
+                <option value="">Species</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="rabbit">Rabbit</option>
+                <option value="reptile">Reptile</option>
+              </select>
+              <Button type="submit" variant="outline-success">
+                Search
+              </Button>
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
-
 
 export default NavbarComponent;
